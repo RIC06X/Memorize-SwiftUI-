e5cac5ce9f3f7c130a939e09ae856ecd5393d738
Yanjie Xu <yanjiexu06@gmail.com>
2020-07-16 12:39:14 +0800

    Initial Commit

---
+++ Memorize/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Memorize
+//
+//  Created by Yanjie Xu on 2020/7/16.
+//  Copyright © 2020 Yanjie Xu. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
